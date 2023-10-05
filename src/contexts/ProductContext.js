0220417f59fe93e3dd2aa2b53c04655653e1545d
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-05 23:11:08 +0300

    Add product context

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from "react";
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+export const ProductContext = createContext();
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
 };
 
-export default ProductContext;
+export default ProductProvider;
