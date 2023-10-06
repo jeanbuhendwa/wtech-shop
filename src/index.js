0765483078be2f1fd9e69da771178f6afc2522e5
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-06 14:38:05 +0300

    Update indexjs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import ProductProvider from "./contexts/ProductContext";
+import SidebarProvider from "./contexts/SidebarContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ProductProvider>
+  <SidebarProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </SidebarProvider>
 );
