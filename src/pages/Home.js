88b0d7a9af0f843a72c2b0576ad3442e988d58ef
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-06 14:06:03 +0300

    Update Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { ProductContext } from "../contexts/ProductContext";
 import Product from "../components/Product";
+import "./pageStyle.css";
 const Home = () => {
   const { products } = useContext(ProductContext);
 
@@ -15,11 +16,7 @@ const Home = () => {
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
             {filteredItems.map((item) => {
-              return (
-                <div className="w-full h-[300px] bg-gray-200" key={item.id}>
-                  {item.title}
-                </div>
-              );
+              return <Product item={item} key={item.id} />;
             })}
           </div>
         </div>
