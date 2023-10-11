9246e5a8bdc10eeb7236f807a59a784b8ef70576
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-11 15:59:00 +0300

    Add cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SidebarContext } from "../contexts/SidebarContext";
+import { BsBag } from "react-icons/bs";
 
 const Header = () => {
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
   return (
     <div>
       <div>Header</div>
-      <div>open/close Sidebar</div>
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className="cursor-pointer flex relative"
+      >
+        <BsBag className="text-3xl" />
+      </div>
     </div>
   );
 };
