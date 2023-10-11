95d0be0083c711fe2cd1c9353d21966c26e8cd51
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-11 15:59:15 +0300

    Add sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,21 @@ import { SidebarContext } from "../contexts/SidebarContext";
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
   return (
-    <div className="w-full bg-[#F2DFD8] top-0 h-full shadow-2xl">Sidebar</div>
+    <div
+      className={` ${
+        isOpen ? "right-0" : "-right-full"
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+    >
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="uppercase text-l font-semibold">Shopping Cart (0) </div>
+        <div
+          onClick={handleClose}
+          className="cursor-pointer w-8 h-8 flex justify-center items-center"
+        >
+          <IoMdArrowForward className="text-2xl" />
+        </div>
+      </div>
+    </div>
   );
 };
 
