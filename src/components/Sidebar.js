2de63f0f6441ed8abde8393bd55def4d27c0381c
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-12 22:00:02 +0300

    Update sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,12 +2,13 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { IoMdArrowForward } from "react-icons/io";
 import { Fitrash2 } from "react-icons/fi";
+import { FiTrash } from "react-icons/fi";
 import CartItem from "../components/CartItem";
 import { SidebarContext } from "../contexts/SidebarContext";
 import { CartContext } from "../contexts/CartContext";
 
 const Sidebar = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, clearCart } = useContext(CartContext);
   const { isOpen, handleClose } = useContext(SidebarContext);
   return (
     <div
@@ -28,6 +29,19 @@ const Sidebar = () => {
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
+      </div>
+      <div className="flex flex-col gap-y-3 py-4 mt-4">
+        <div className="flex w-full justify-between items-center">
+          <div className="uppercase font-semibold">
+            <span className="mr-2">Total:</span>$ 1000
+          </div>
+          <div
+            onClick={clearCart}
+            className="cursor-pointer py-4 bg-[#8C5245] text-white w-12 h-12 flex justify-center items-center text-2xl font-medium"
+          >
+            <FiTrash />
+          </div>
+        </div>
       </div>
     </div>
   );
