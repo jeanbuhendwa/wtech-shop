3a25d1d6053604d8fe68414ee087b01cf43b0244
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-12 22:25:40 +0300

    Update cartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,7 +4,8 @@ import { CartContext } from "../contexts/CartContext";
 
 const CartItem = ({ item }) => {
   const { id, title, image, price, amount } = item;
-  const { removeFromCart } = useContext(CartContext);
+  const { removeFromCart, increaseAmount, decreaseAmount } =
+    useContext(CartContext);
 
   return (
     <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
@@ -26,13 +27,19 @@ const CartItem = ({ item }) => {
           </div>
           <div className="flex  gap-x-2 h-[36px] text-sm items-center">
             <div className="flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium">
-              <div className="flex-1 flex justify-center items-center cursor-pointer">
+              <div
+                onClick={() => decreaseAmount(id)}
+                className="flex-1 flex justify-center items-center cursor-pointer"
+              >
                 <IoMdRemove />
               </div>
               <div className="h-full flex justify-center items-center px-2">
                 {amount}
               </div>
-              <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+              <div
+                onClick={() => increaseAmount(id)}
+                className="flex-1 h-full flex justify-center items-center cursor-pointer"
+              >
                 <IoMdAdd />
               </div>
             </div>
