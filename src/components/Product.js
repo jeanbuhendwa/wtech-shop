4dff750c58eb2bc6224f885ace4a33692e7fd978
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-12 15:00:19 +0300

    Update product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { BsPlus, BsEyeFill } from "react-icons/bs";
+import { CartContext } from "../contexts/CartContext";
 
 const Product = ({ item }) => {
   console.log(item);
+
+  const { addToCart } = useContext(CartContext);
   const { id, image, category, title, price } = item;
   return (
     <section>
@@ -18,7 +21,7 @@ const Product = ({ item }) => {
           </div>
         </div>
         <div className="absolute top-6 -right-11 group-hover:right-3 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group group-hover:opacity-100 transition-all duration-300">
-          <button>
+          <button onClick={() => addToCart(item, id)}>
             <div className="flex items-center justify-center w-10 h-10 text-[#F2DFD8] bg-[#8C4D3F]">
               <BsPlus className="text-3xl" />
             </div>
