5c71026a6373bfea2ea48e67c2a7a50d48adf608
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-12 22:25:56 +0300

    Update cartContext

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -34,9 +34,41 @@ const CartProvider = ({ children }) => {
   const clearCart = () => {
     setCart([]);
   };
+
+  const increaseAmount = (id) => {
+    const cartIten = cart.find((item) => item.id === id);
+    addToCart(cartIten, id);
+  };
+
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: item.amount - 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    }
+    if (cartItem.amount < 2) {
+      removeFromCart(id);
+    }
+  };
+
   return (
     <CartContext.Provider
-      value={{ cart, addToCart, removeFromCart, clearCart }}
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        increaseAmount,
+        decreaseAmount,
+      }}
     >
       {children}
     </CartContext.Provider>
