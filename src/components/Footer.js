6e4c2b07f87acfe8d25a5267d5e3638f1fef7f4a
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-17 12:05:37 +0300

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; WTech shop. All right reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
