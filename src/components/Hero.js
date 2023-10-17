f5f609e003bd5c72f302405dd928e863d7e452b0
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-17 12:05:55 +0300

    Update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import WomanImg from "../img/woman_hero.png";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
+      <div className="container mx-auto flex justify-around h-full">
+        <div className="flex flex-col justify-center">
+          <div className="font-semibold flex items-center uppercase">
+            <div className="w-10 h-[2px] bg-[#8C5245] mr-3"></div>New Trend
+          </div>
+          <h1 className="text-[70px] leading-[1.1] font-light mb-4">
+            AUTUMN SALE STYLISH
+            <br />
+            <span className="font-semibold">WOMEN</span>
+          </h1>
+          <Link
+            to={"/"}
+            className="self-start uppercase font-semibold border-b-2 border-primary"
+          >
+            Discover More
+          </Link>
+        </div>
+        <div className="hidden lg:block">
+          <img src={WomanImg} alt="woman" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
